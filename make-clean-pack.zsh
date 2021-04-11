4a07373e8648f3662545a34b4f2e185e225a4876
nejni-marji <nejni.marji@gmail.com>
2021-04-11 13:32:07 -0500

    Automated commit

--- make-clean-pack.zsh
+++ make-clean-pack.zsh
@@ -18,6 +18,8 @@ mc_get_hash() {
 mc_get_hash $mc_default > cache/hash_base.txt
 mc_get_hash $mc_full > cache/hash_mine.txt
 
+printf '%0.s#' {1..$COLUMNS} ; print
+
 # get changed files
 local -a file_diff=(${(f)"$(comm -13 cache/hash_base.txt cache/hash_mine.txt | cut -c37-)"})
 
@@ -27,15 +29,18 @@ setopt RC_EXPAND_PARAM
 # do the actual copy from full to clean
 rsync -Rav $mc_full/./${file_diff} $mc_clean/
 
+printf '%0.s#' {1..$COLUMNS} ; print
+
 cd $mc_clean ; zip -rv ../$mc_clean.zip * ; cd -
+
+printf '%0.s#' {1..$COLUMNS} ; print
 
 git add -A
 git commit -m 'Automated commit'
 git push origin master
 
-
-
 printf '%0.s#' {1..$COLUMNS} ; print
+
 echo 'Attempting to update server.properties automatically...'
 commit_id=$(git log | head -n1 | cut -c8-)
 mc_url="https://github.com/nejni-marji/minecraft-extras/raw/$commit_id/my_pack_clean.zip"
